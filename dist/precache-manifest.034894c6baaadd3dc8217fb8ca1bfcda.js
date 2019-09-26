f65d11a91139129cf64beaaa915b62de444e2ab3
Marko Siilak <marko@siilak.com>
2019-09-26 12:53:29 +0300

    Build

---
+++ dist/precache-manifest.034894c6baaadd3dc8217fb8ca1bfcda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6567738d99ce9ca1b01d",
+    "url": "/js/chunk-vendors.bbd8fc5c.js"
+  },
+  {
+    "revision": "e2d1cb8aaf4c87c05eda",
+    "url": "/js/app.b1af0852.js"
+  },
+  {
+    "revision": "9679b2a697ae75b13ddb60c99fa0c7c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6be2e18a982fa6fdea2521a20c38cc2",
+    "url": "/img/tabloo.c6be2e18.svg"
+  },
+  {
+    "revision": "7e88eb73932332a5664b0dbc34ce7996",
+    "url": "/img/rong.7e88eb73.svg"
+  },
+  {
+    "revision": "e7674b321c315884778972e81019ce84",
+    "url": "/img/logo.e7674b32.svg"
+  },
+  {
+    "revision": "3f1ee6b4afa0de553e03beda06f97b40",
+    "url": "/img/juht.3f1ee6b4.svg"
+  },
+  {
+    "revision": "6567738d99ce9ca1b01d",
+    "url": "/css/chunk-vendors.a031dfb3.css"
+  },
+  {
+    "revision": "e2d1cb8aaf4c87c05eda",
+    "url": "/css/app.7fedb4f3.css"
+  }
+];
